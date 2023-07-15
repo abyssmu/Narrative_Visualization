c111b69242fe20dffb5faf8ca9b59918c0a12ce6
abyssmu <abyssmu@hotmail.com>
2023-07-15 11:22:57 -0500

    Started d3 implementation

--- cleaning.py
+++ cleaning.py
@@ -9,6 +9,55 @@ df = pd.DataFrame()
 for filename in files:
 	temp = pd.read_csv(os.path.join(path, filename))
 
+	try:
+		temp = temp.drop(['Country_Region',
+							'Last_Update',
+							'Lat',
+							'Long_',
+							'Recovered',
+							'Active',
+							'FIPS',
+							'Incident_Rate',
+							'Total_Test_Results',
+							'People_Hospitalized',
+							'Case_Fatality_Ratio',
+							'UID',
+							'ISO3',
+							'Testing_Rate',
+							'Hospitalization_Rate',
+							'Date',
+							'People_Tested',
+							'Mortality_Rate'], axis = 1)
+	except:
+		temp = temp.drop(['Country_Region',
+							'Last_Update',
+							'Lat',
+							'Long_',
+							'Recovered',
+							'Active',
+							'FIPS',
+							'Incident_Rate',
+							'Total_Test_Results',
+							'People_Hospitalized',
+							'Case_Fatality_Ratio',
+							'UID',
+							'ISO3',
+							'Testing_Rate',
+							'Hospitalization_Rate'], axis = 1)
+
+	temp[['Month', 'Day', 'Year']] = filename[:-4].split('-')
+
 	df = pd.concat([df, temp])
 
-df.to_csv('total_us_daily_compiled.cvs', index = False)
+territories = ['American Samoa',
+					'Diamond Princess',
+					'Grand Princess',
+					'Guam',
+					'Northern Mariana Islands',
+					'Puerto Rico',
+					'Virgin Islands']
+df = df[~df['Province_State'].isin(territories)]
+
+df = df.sort_values(by = ['Year', 'Month', 'Day', 'Province_State'])
+
+df.to_csv('total_us_daily_compiled.csv', index = False)
--- d3_implementation.js
+++ d3_implementation.js
@@ -2,9 +2,18 @@ async function init()
 {
 	const data = await d3.csv('total_us_daily_compiled.csv');
 
-	console.log(data);
-
 	d3.select('body')
+		.selectAll('p')
+		.data(data)
+		.enter()
 		.append('p')
-			.html(data[0].Date);
+			.html(function(d)
+				{
+					return 'State: ' + d.Province_State + ', ' +
+							'Confirmed: ' + d.Confirmed + ', ' +
+							'Deaths: ' + d.Deaths + ', ' +
+							'Day: ' + d.Day + ', ' +
+							'Month: ' + d.Month + ', ' +
+							'Year: ' + d.Year;
+				});
 }
